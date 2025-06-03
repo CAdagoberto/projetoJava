e4bf056e6d7f8ac8cf600e3596a7b3d00d81597e
JBdevCristian <jbcristian78@gmail.com>
2025-06-03 16:11:35 -0300

    Transformando projeto em node

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const bodyParser = require('body-parser')
+const path = require('path')
+
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.get("/cardapio", (req, res) => {
+    res.render("cardapio")
+})
+
+
+app.listen(8080, () => {
+    console.log("Servidor rodando")
+})
